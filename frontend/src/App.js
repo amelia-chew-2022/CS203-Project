40ab0a2ee8cd3c99e6e0df6e52345027c510b759
Loh Oliver <oliver.loh.2022@scis.smu.edu.sg>
2023-09-13 16:35:19 +0800

    Add SeatSelect Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Switch, { Routes } from 'react-router';
-import Home from './pages/Home';
-// import EventInfo from './pages/EventInfo';
+import Home from "./pages/Home";
+import SeatSelect from "./pages/SeatSelect"; // Corrected component name
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App(){
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" component={Home} />
-                {/* <Route path="/eventinfo" component={EventInfo} /> */}
-                {/* <Route path="/login" component={Login}/> */}
-            </Routes>
-        </Router>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/seatselect" element={<SeatSelect />} /> {/* Corrected path */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import reportWebVitals from './reportWebVitals';
-// import App from './App';
-import Home from './pages/Home';
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import ReactDOM from "react-dom";
+
+//import "./index.css";
+
+import App from "./App";
+
+ReactDOM.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+);
