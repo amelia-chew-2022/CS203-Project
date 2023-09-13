ced637964ccebad433ff80bd851ad43ac8da21fd
Amelia Chew <amelia.chew.2022@scis.smu.edu.sg>
2023-09-13 20:48:02 +0800

    installed react-step-progress-bar re-resizable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Switch, { Routes } from 'react-router';
 import Home from './pages/Home';
-// import EventInfo from './pages/EventInfo';
+import EventInfo from './pages/EventInfo';
+import Login from './pages/Login';
+import SeatSelect from './pages/SeatSelect';
 
-function App(){
+const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" component={Home} />
-                {/* <Route path="/eventinfo" component={EventInfo} /> */}
-                {/* <Route path="/login" component={Login}/> */}
+                <Route path="/" element={<Home/>}/>
+                <Route path="/eventinfo" element={<EventInfo/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/seatselect" element={<SeatSelect/>}/>
             </Routes>
         </Router>
     );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import reportWebVitals from './reportWebVitals';
-// import App from './App';
-import Home from './pages/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+//import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
