db6ea2a69ddaeec1adae51a2387858617728f6e0
Shawn Chia <shawn.chia.2022@scis.smu.edu.sg>
2023-09-20 21:57:41 +0800

    delete file

--- Ticketing/src/main/java/csd/week5/ticket/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package csd.week5.ticket;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository <Ticket, Long> {
-
-}
